b83d618862f6adb0464b20b11c2f8272fa55f204
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-01-03 00:25:09 -0600

    changed the about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,13 +12,14 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center page-title font">About this App</h1>
+
       <p>This is a weather application built with React. Built in order to strengthen my knowledge of React.</p>
       <p>
         Here are some of the tools I used:
       </p>
       <ul>
         <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used produced by Facebook.
         </li>
         <li>
           <a href="http://openweathermap.org">Open Weather Map</a> - This is the API used in order to get weather updates.
